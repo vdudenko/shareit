be0d3411031016bb14601378130d393f795e9b47
Dudenko Vadim <dudenko.vadim@gmail.com>
2025-09-03 17:03:11 +0300

    first commit

--- src/main/java/ru/yandex/practicum/shareit/ShareItApplication.java
+++ src/main/java/ru/yandex/practicum/shareit/ShareItApplication.java
@@ -1,4 +1,11 @@
 package ru.yandex.practicum.shareit;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class ShareItApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ShareItApplication.class, args);
+    }
 }
